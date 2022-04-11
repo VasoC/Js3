27fe250eef799ca1a312ab42438e0c52dbacb0b1
VasoC <vasochkhartishvili@gmail.com>
2022-04-11 08:18:17 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // ამ დავალებაში ცოტა ვერ გავარკვიე
+// დავალება 1
 const currentDay = new Date().getDay();
 
 switch (currentDay) {
@@ -27,3 +28,39 @@ switch (currentDay) {
     console.log("შეცდომა");
     break;
 }
+// დავალება #2
+for (var i = 0; i < 100; i++) {
+  console.log(i);
+}
+// დავალება 3
+var i = 0;
+while (i < 50) {
+  console.log(i);
+  i++;
+}
+// დავალება 4
+do {
+  console.log(i);
+  i++;
+} while (i < 150);
+// დავალება 5
+let myArray = [];
+
+for (let i = 1; i <= 1000; i++) {
+    myArray.push(i*i)
+}
+console.log(myArray)
+
+do {
+    i++
+    myArray.push(i*i)
+} while (i < 1000);
+
+console.log(myArray);
+
+while (i < 1000){
+    i++
+    myArray.push(i*i)
+}
+
+console.log(myArray);
