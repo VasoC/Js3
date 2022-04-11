ddb48deaccc7eeb01dc4c7f8108edf8506eda0d5
VasoC <vasochkhartishvili@gmail.com>
2022-04-11 08:10:24 +0400

    Update!!!

---
+++ main.js
@@ -0,0 +1,29 @@
+// ამ დავალებაში ცოტა ვერ გავარკვიე
+const currentDay = new Date().getDay();
+
+switch (currentDay) {
+  case 0:
+    console.log("კვირა");
+    break;
+  case 1:
+    console.log("ორშაბათი");
+    break;
+  case 2:
+    console.log("სამშაბათი");
+    break;
+  case 3:
+    console.log("ოთხშაბათი");
+    break;
+  case 4:
+    console.log("ხუთშაბათი");
+    break;
+  case 5:
+    console.log("პარასკევი");
+    break;
+  case 6:
+    console.log("შაბათი");
+    break;
+  default:
+    console.log("შეცდომა");
+    break;
+}
